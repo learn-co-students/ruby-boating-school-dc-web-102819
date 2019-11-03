894cf1d666877cdaa3f52adb1101cba4be904b1b
Kyle <kylepcole@gmail.com>
2019-11-03 13:40:14 -0500

    lab work

--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,15 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+kyle = Student.new("Kyle")
+kevin = Student.new("Kevin")
+
+chris = Instructor.new("Chris")
+karen = Instructor.new("Karen")
+
+boating101 = kyle.add_boating_test("Boating 101", "open", chris)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
