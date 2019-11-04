a37ceec2fd6e9c187e853cf2672fa68a58ca44d1
Marija Stojanovic <marijastojanovic5@yahoo.com>
2019-11-03 21:13:02 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,40 @@
 class Instructor
+    attr_reader :name
+    @@all =[]
+    def initialize (name)
+        @name= name
+        @@all << self
+    end
+    def self.all 
+        @@all
+    end 
+   
+    def pass_student(student, boating_test_name)
+        test1 = find_test(student, boating_test_name)
+        if test1
+            test1.boating_status = "passed"
+            return test1
+        else
+            new_test = BoatingTest.new(student, test_name, "passed", self)  
+            new_test  
+        end
+    end
+        
+    def fail_student(student, boating_test_name)
+        test1 = find_test(student, boating_test_name)
+        if test1
+            test1.boating_status = "failed"
+            return test1
+        else
+            new_test = BoatingTest.new(student, test_name, "failed", self)  
+            new_test  
+        end
 
-end
+    end
+
+end    
+     
+          
+            
+
+
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,38 @@
 class Student
+    
+    attr_accessor :first_name
+    @@all=[]
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+    def self.all
+        @all
+    end
+    def add_boating_test(boating_test_name, boating_teat_status,instructor)
+        BoatingTest.new(self,boating_test_name, boating_teat_status,instructor)
+    end
+    
+    def self.find_student(name)
+        self.all.find do |student|
+          student.first_name == name
+        end
+      end
+
+   
+    def grade_percentage
+        student_tests= BoatingTest.all.find_all do|test| 
+            test.student == self
+        end
+        passed_test= student_tests.select do |test|
+            test.boating_test_status == "passed"
+        end
+        percentage = (passed_tests.count.to_f/student_tests.count.to_f)*100
+        
+        return percentage
+       
+      end
+      
+
 
 end
